cf273112b754ad99106a2dfa4afe8c230dbc21fc
xiashura <plexanov@protonmail.com>
2023-04-29 10:13:09 +0300

    init vault charts value and scirpts

---
+++ scripts/vault-init.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+helm upgrade --install --namespace vault \
+  --create-namespace \
+  --repo https://helm.releases.hashicorp.com \
+  --values ./charts/vault/value.yml vault vault  
+kubectl -n vault exec vault-0 -- vault operator init  -key-shares=1  -key-threshold=1  -format=json > cluster-keys.json
+jq -r ".unseal_keys_b64[]" cluster-keys.json
+VAULT_UNSEAL_KEY=$(jq -r ".unseal_keys_b64[]" cluster-keys.json)
+kubectl exec vault-0 -- vault operator unseal $VAULT_UNSEAL_KEY
+kubectl -n vault exec vault-0 -- vault operator unseal $VAULT_UNSEAL_KEY
+kubectl -n vault exec -ti vault-1 -- vault operator raft join http://vault-0.vault-internal:8200
+kubectl -n vault exec -ti vault-1 -- vault operator unseal $VAULT_UNSEAL_KEY
