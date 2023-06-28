392b48010e01b52e81f8e26b1a49883af3c31dbd
xiashura <plexanov@protonmail.com>
2023-06-28 18:05:35 +0300

    add commad auto put ip for datasource

--- scripts/install-grafana.sh
+++ scripts/install-grafana.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+export PROMETHEUS_SERVER=$(kubectl -n prometheus get svc prometheus-server -o jsonpath="{.spec.clusterIP}")
+
+export LOKI_SERVER=$(kubectl -n loki get svc loki-gateway -o jsonpath="{.spec.clusterIP}")
+
+sed -i "s/PROMETHEUS_SERVER/$PROMETHEUS_SERVER/g" charts/grafana/value.yml
+sed -i "s/LOKI_SERVER/$LOKI_SERVER/g" charts/grafana/value.yml
+
 helm upgrade --install \
   --wait \
   --create-namespace \
@@ -7,3 +14,7 @@ helm upgrade --install \
   --repo https://grafana.github.io/helm-charts \
   --values ./charts/grafana/value.yml \
   grafana grafana 
+
+
+sed -i "s/$PROMETHEUS_SERVER/PROMETHEUS_SERVER/g" charts/grafana/value.yml
+sed -i "s/$LOKI_SERVER/LOKI_SERVER/g" charts/grafana/value.yml
